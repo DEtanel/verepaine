8db71e73178d733942f8333b9669d08f3c58c1bb
DEtanel <116672331+DEtanel@users.noreply.github.com>
2023-03-19 20:01:41 +0200

    Add files via upload
    Added visual dataview

--- main.py
+++ main.py
@@ -1,113 +1,232 @@
-import csv 
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QTableWidgetItem, QMainWindow , QFileDialog
+from PyQt5.QtCore import QCoreApplication
+from mainwindow3 import Ui_MainWindow
+import sys
+import pyowm
+import csv
 import matplotlib.pyplot as plt
-# import PyQt5                      / GUI Coming soon as possible. It`a hard work, bro.
-# from bs4 import BeautifulSoup     / Web scraping library, extract data from html/xml
-# import requests                   / Coming soon in future if possible...
-
-def draw():
-    data_list = []
-    with open("date.txt") as file3:
-        file_reader3 = csv.reader(file3)
-        for row in file_reader3:
-            data_list.append(row[0])
-
-    pulse = []
-    with open("pulse.txt") as file:
-        file_reader = csv.reader(file)
-        for row in file_reader:
-            pulse.append(row[0])
-
-    sys_list = []
-    with open("sys.txt") as file1:
-        file_reader1 = csv.reader(file1)
-        for row in file_reader1:
-            sys_list.append(row[0])
-
-    dia_list = []
-    with open("dia.txt") as file2:
-        file_reader2 = csv.reader(file2)
-        for row in file_reader2:
-            dia_list.append(row[0])
 
 
-    x = data_list
-    y1 = sys_list
-    y2 = dia_list
-    y3 = pulse
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.push_Save_clicked)
+        self.pushButton_2.clicked.connect(self.push_Graph_clicked)
+        self.pushButton_3.clicked.connect(self.push_Settings_clicked)    
+        self.pushButton_4.clicked.connect(QCoreApplication.instance().quit)
 
-    plt.title('Blood Pressure & Pulse')
-    plt.xlabel(xlabel='Date')
-    plt.ylabel(ylabel='Value')
-    plt.plot(x, y1, label='sys', color='red')
-    plt.plot(x, y2, label='dia', color='blue')
-    plt.plot(x, y3, label='pulse', color='green')
-    plt.grid()
-    plt.draw()
-    plt.legend(loc='upper left')
-    plt.show()
+        self.read_files()
+
+    def read_files(self):
+        # Open files for visual showing in QTableWidget
+        with open('date_1.txt', 'r') as f1, open('sys_1.txt', 'r') as f2, open('dia_1.txt', 'r') as f3, open('pulse_1.txt','r') as f4, open('weather_pressure_1.txt','r') as f5:
+            data1 = f1.readlines()
+            data2 = f2.readlines()
+            data3 = f3.readlines()
+            data4 = f4.readlines()
+            data5 = f5.readlines()
+
+        # Set the numbers and names of columns in the table
+        rows = max(len(data1), len(data2), len(data3), len(data4), len(data5))
+        self.tableWidget.setRowCount(rows)
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setHorizontalHeaderLabels(['Data','Sys','Dia','Pulse','Weather pressure'])
+
+        # Add the data to the table
+        for row in range(rows):
+            # Add the data from date.txt
+            if row < len(data1):
+                item1 = QTableWidgetItem(data1[row].strip())
+            else:
+                item1 = QTableWidgetItem('')
+            self.tableWidget.setItem(row, 0, item1)
+
+            # Add the data from sys.txt
+            if row < len(data2):
+                item2 = QTableWidgetItem(data2[row].strip())
+            else:
+                item2 = QTableWidgetItem('')
+            self.tableWidget.setItem(row, 1, item2)
+
+            # Add the data from dia.txt
+            if row < len(data3):
+                item3 = QTableWidgetItem(data3[row].strip())
+            else:
+                item3 = QTableWidgetItem('')
+            self.tableWidget.setItem(row, 2, item3)
+
+            # Add the data from pulse.txt
+            if row < len(data4):
+                item4 = QTableWidgetItem(data4[row].strip())
+            else:
+                item4 = QTableWidgetItem('')
+            self.tableWidget.setItem(row, 3, item4)
+
+            # Add the data from weather_pressure.txt
+            if row <  len(data5):
+                item5 = QTableWidgetItem(data5[row].strip())
+            # else:
+            #     item5 = QTableWidgetItem('')
+            self.tableWidget.setItem(row, 4, item5)
 
 
-# import pandas as pd                 # Data-analytiikka library/modul
-# import matplotlib.pyplot as plt     # Draw library
-# import PyQt5                      / GUI Coming soon as possible. It`a hard work, bro.
-# from bs4 import BeautifulSoup     / Web scraping library, extract data from html/xml
-# import requests                   / Coming soon in future if possible...
 
-# Funktio: Tallentaminen SYS,DIA,PULE,DATA -tiedoston uusi riville csv-tiedoston
-# def add(veri_sys, veri_dia, veri_pulse, veri_data):
-#    with open('tiedot.csv', 'a', encoding='utf-8') as file:  # Avaa tiedosto "a"(Add)-muodossa
-#        paine_tiedot = f'{veri_data};{veri_sys};{veri_dia};{veri_pulse}\n'
-#        file.write(paine_tiedot)
-#       print('Tallenettu CSV-tiedoston!')
-
-
-# Pääohjelma
-while True:
-    command = int(input("""
-        Verepaine tallenusohjelma! Blood pressure mega-analysing soft!
+    def push_Save_clicked(self):
+        owm =pyowm.OWM('f8c43bbd601d39c177afabec2d050d04')
+        mgr = owm.weather_manager()
+        weather_pressure = mgr.weather_at_place('Helsinki').weather.pressure
+        pressure_value=str(weather_pressure['press'])
+        
+        sys_value = self.textEdit.text()
+        dia_value = self.textEdit_2.text()
+        pulse_value = self.textEdit_3.text()
+        data_value = self.textEdit_4.text() 
     
-    Valitse numero / Take your choice, please:
-    1 - Lisää mittaustiedot     / Add new Blood pressure measurements-data
-    2 - Kuva                    / Draw the Graphic of collected date
-    3 - Lopeta ohjelma          / Exit!
-    Numero?: """))
 
-    if command == 1:  # Lisää verepainelaite mittaukset tiedoston
-        veri_sys = input('SYS: ')
-        with open("sys.txt",'a') as sysfile:3
-            sysfile.write(str(veri_sys) + '\n')
-            print('SYS-mittaus tallennettu!')
-        sysfile.close()
+        with open('sys.txt', 'a') as sysfile:
+            sysfile.write(str(sys_value) + '\n')
+            print('Sys-tieto tallennettu')
+            sysfile.close()
 
-        veri_dia = input('DIA: ')
-        with open("dia.txt",'a') as diafile:
-            diafile.write(str(veri_dia) + '\n')
-            print('DIA-muttaus tallennettu!')
-        diafile.close()
+        with open('dia.txt', 'a') as diafile:
+            diafile.write(str(dia_value) + '\n')
+            print('Dia-tieto tallennettu')
+            diafile.close()
 
-        veri_pulse = input('Pulse: ')
-        with open("pulse.txt",'a') as pulsefile:
-            pulsefile.write(str(veri_pulse) + '\n')
-            print('Pulse-tieto tallennetuu!')
-        pulsefile.close()
+        with open('pulse.txt', 'a') as pulsefile:
+            pulsefile.write(str(pulse_value) + '\n')
+            print('Pulse-tieto tallennettu')
+            pulsefile.close()
+        
+        with open('date.txt', 'a') as datefile:
+            datefile.write(str(data_value) + '\n')
+            print('Päivä-tieto tallennettu')
+            datefile.close()
+        
+        with open('weather_pressure.txt', 'a') as pressdatafile:
+            pressdatafile.write(str(pressure_value) + '\n')
+            print('Weather pressure data tallennettu')
+            pressdatafile.close()
+        
+        
+        self.save_to_reversed_files()
+        self.read_files()
 
-        veri_data = input('Date(pp.kk.vv): ')
-        with open("date.txt",'a') as datefile:
-            datefile.write(str(veri_data) + '\n')
-            print('Date tallennettu')
-        datefile.close()
+    def save_to_reversed_files(self):
+        # Array converter for reverse data showing in QTableWidget
+        array = list()
+        with open("date.txt", 'r') as date:
+            for line in date:
+                array.insert(0,line.strip())
+        date.close
+        date_1 = open('date_1.txt','w')
+        for i in range (0, len(array)):
+            date_1.write(array[i]+'\n')
+        date_1.close
+        array.clear()
 
-#        add(veri_sys, veri_dia, veri_pulse, veri_data)
 
-    elif command == 2:  # Kuva
-        draw()
+        with open("sys.txt", 'r') as sys:
+            for line in sys:
+                array.insert(0,line.strip())
+        sys.close
+        sys_1 = open('sys_1.txt','w')
+        for i in range (0, len(array)):
+            sys_1.write(array[i]+'\n')
+        sys_1.close
+        array.clear()
 
-#   elif command == 3:  # tiedosto.csv luominen
-#       with open('tiedot.csv', 'w', encoding='utf-8') as fle:
-#            line = f'Data;SYS;DIA;PULSE\n'  # taulukon sarake-nimet
-#           file.write(line)
-#           print('Uusi CSV-tiedosto luotu!')
 
-    elif command == 3:  # Exit
-        print('Kiitos käynnistä ja tsemppiä!')
-        break
+        with open("dia.txt", 'r') as dia:
+            for line in dia:
+                array.insert(0,line.strip())
+        dia.close
+        dia_1 = open('dia_1.txt','w')
+        for i in range (0, len(array)):
+            dia_1.write(array[i]+'\n')
+        dia_1.close
+        array.clear()
+
+
+        with open("pulse.txt", 'r') as pulse:
+            for line in pulse:
+                array.insert(0,line.strip())
+        pulse.close
+        pulse_1 = open('pulse_1.txt','w')
+        for i in range (0, len(array)):
+            pulse_1.write(array[i]+'\n')
+        pulse_1.close
+        array.clear()
+
+        with open("weather_pressure.txt", 'r') as wpr:
+            for line in wpr:
+                array.insert(0,line.strip())
+        wpr.close
+        wpr_1 = open('weather_pressure_1.txt','w')
+        for i in range (0, len(array)):
+            wpr_1.write(array[i]+'\n')
+        wpr_1.close
+        array.clear()
+
+
+    def push_Settings_clicked(self):
+        self.openWindow()
+        # self.setupUi.lineEdit_city 
+    
+    def push_About_clicked(self):
+        self.openWindow2()
+
+    def push_Graph_clicked(self):  
+        with open('date.txt', 'r') as filedata:
+        # Read the contents of the file and split it by newline character
+            data_list = filedata.read().split('\n')
+        filedata.close()
+
+        with open('sys.txt', 'r') as filesys:
+        # Read the contents of the file and split it by newline character
+            int_list =[]
+            sys_list = filesys.read().split('\n')
+        filesys.close()
+    
+        with open('dia.txt', 'r') as filedia:
+        # Read the contents of the file and split it by newline character
+            dia_list = filedia.read().split('\n')
+        filedia.close()
+
+        with open('weather_pressure.txt', 'r') as fileweather:
+        # Read the contents of the file and split it by newline character
+            weather_list = fileweather.read().split('\n')
+        fileweather.close()
+
+
+        x = data_list
+        y1 = sys_list
+        #y2 = dia_list
+        #y3 = weather_list
+
+
+
+
+        plt.title('Blood Pressure & Pulse')
+        plt.xlabel(xlabel='Date')
+        plt.xticks(rotation=45)
+        plt.ylabel(ylabel='Value')
+        plt.plot(x, y1, label='sys', color='red')
+
+        plt.grid()
+        plt.draw()
+        plt.legend(loc='upper left')
+        plt.show()
+
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
+
+#  pyuic5 -o pyfilename.py design.ui  # Convert QT Desigenrfile to .py
