3a2cb6d30ed65ce5b7ed381f54119bace297328b
DEtanel <116672331+DEtanel@users.noreply.github.com>
2023-03-27 17:21:05 +0300

    Add files via upload

--- start.py
+++ start.py
@@ -1,12 +1,13 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QTableWidgetItem, QMainWindow , QFileDialog, QLCDNumber
+from PyQt5.QtWidgets import QDialog, QApplication, QTableWidgetItem, QMainWindow , QFileDialog
 from PyQt5.QtCore import QCoreApplication
 from mainwindow3 import Ui_MainWindow
+import os
 import sys
 import pyowm
-# import matplotlib.pyplot as plt
-
-
+import csv
+import matplotlib.pyplot as plt
+print("Working dir:", os.getcwd())
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -17,8 +18,29 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.pushButton_3.clicked.connect(self.push_Settings_clicked)    
         self.pushButton_4.clicked.connect(QCoreApplication.instance().quit)
 
+        self.read_files()
+
+    def read_files(self):
+
+#### ---------- IF FILES NOT EXSITS ----------------- #####        
+        
+        files = ["date_1.txt", "date.txt", "sys_1.txt", "sys.txt", "dia.txt", "dia_1.txt", "pulse.txt", "pulse_1.txt","weather_pressure.txt", "weather_pressure_1.txt"]
+        for file in files:
+            try:
+            # Try to open the file in read mode
+                with open(file, 'r') as f:
+                # File exists, do something with it
+                    print(f"File {file} exists!")
+            except FileNotFoundError:
+            # File doesn't exist, create it
+                with open(file, 'w') as f:
+                    print(f"File {file} created.")
+
+#### ---------- END OF CHECK EXISTING FILES ---------#####
+
+
         # Open files for visual showing in QTableWidget
-        with open('date.txt', 'r') as f1, open('sys.txt', 'r') as f2, open('dia.txt', 'r') as f3, open('pulse.txt','r') as f4, open('weather_pressure.txt','r') as f5:
+        with open('date_1.txt', 'r') as f1, open('sys_1.txt', 'r') as f2, open('dia_1.txt', 'r') as f3, open('pulse_1.txt','r') as f4, open('weather_pressure_1.txt','r') as f5:
             data1 = f1.readlines()
             data2 = f2.readlines()
             data3 = f3.readlines()
@@ -64,91 +86,182 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             # Add the data from weather_pressure.txt
             if row <  len(data5):
                 item5 = QTableWidgetItem(data5[row].strip())
-            else:
-                item5 = QTableWidgetItem('')
+            # else:
+            #     item5 = QTableWidgetItem('')
             self.tableWidget.setItem(row, 4, item5)
 
 
 
     def push_Save_clicked(self):
+        owm =pyowm.OWM('f8c43bbd601d39c177afabec2d050d04')
+        mgr = owm.weather_manager()
+        weather_pressure = mgr.weather_at_place('Helsinki').weather.pressure
+        pressure_value=str(weather_pressure['press'])
+        
         sys_value = self.textEdit.text()
         dia_value = self.textEdit_2.text()
         pulse_value = self.textEdit_3.text()
         data_value = self.textEdit_4.text() 
     
-        with open('weather_pressure.txt', 'a') as weatherpress_file:
-            weatherpress_file.write(str(pressure_value) + '\n')
-            print(str(pressure_value)+'Weather-tiedot saved')
-            weatherpress_file.close()
 
         with open('sys.txt', 'a') as sysfile:
             sysfile.write(str(sys_value) + '\n')
-            print('Sys-tiedot saved')
+            print('Sys-tieto tallennettu')
             sysfile.close()
 
         with open('dia.txt', 'a') as diafile:
             diafile.write(str(dia_value) + '\n')
-            print('Dia-tiedot saved')
+            print('Dia-tieto tallennettu')
             diafile.close()
 
         with open('pulse.txt', 'a') as pulsefile:
             pulsefile.write(str(pulse_value) + '\n')
-            print('Pulse-tiedot saved')
+            print('Pulse-tieto tallennettu')
             pulsefile.close()
         
         with open('date.txt', 'a') as datefile:
             datefile.write(str(data_value) + '\n')
-            print('Sys-tiedot saved')
+            print('Päivä-tieto tallennettu')
             datefile.close()
-
-        rowCount = self.tableWidget.rowCount()
-        self.tableWidget.insertRow(rowCount)
-        self.tableWidget.setItem(rowCount, 0, QTableWidgetItem(data_value))
-        self.tableWidget.setItem(rowCount, 1, QTableWidgetItem(sys_value))
-        self.tableWidget.setItem(rowCount, 2, QTableWidgetItem(dia_value))
-        self.tableWidget.setItem(rowCount, 3, QTableWidgetItem(pulse_value))
-        self.tableWidget.setItem(rowCount, 4, QTableWidgetItem(pressure_value))
         
-
-
-    def push_Graph_clicked(self):
-        selectedRows = set(index.row() for index in self.tableWidget.selectedIndexes())
-
-        x = []
-        y1 = []
-        y2 = []
-        y3 = []
-
-
-        for row in range(self.tableWidget.rowCount()):
-            if row in selectedRows:
-                x.append(row)
-                y1.append(float(self.tableWidget.item(row, 0).text()))
-                y2.append(float(self.tableWidget.item(row, 1).text()))
-                y3.append(float(self.tableWidget.item(row, 2).text()))
+        with open('weather_pressure.txt', 'a') as pressdatafile:
+            pressdatafile.write(str(pressure_value) + '\n')
+            print('Weather pressure data tallennettu')
+            pressdatafile.close()
         
-        plt.plot(x, y1, label='Field1')
-        plt.plot(x, y2, label='Field2')
-        plt.plot(x, y3, label='Field3')
+        
+        self.save_to_reversed_files()
+        self.read_files()
 
-        plt.xlabel('Row')
-        plt.ylabel('Value')
-        plt.title('Plot blood pressure data & weather pressure')
-        plt.legend
-        plt.show
+    def save_to_reversed_files(self):
+        # Array converter for reverse data showing in QTableWidget
+        array = list()
+        with open("date.txt", 'r') as date:
+            for line in date:
+                array.insert(0,line.strip())
+        date.close
+        date_1 = open('date_1.txt','w')
+        for i in range (0, len(array)):
+            date_1.write(array[i]+'\n')
+        date_1.close
+        array.clear()
+
+
+        with open("sys.txt", 'r') as sys:
+            for line in sys:
+                array.insert(0,line.strip())
+        sys.close
+        sys_1 = open('sys_1.txt','w')
+        for i in range (0, len(array)):
+            sys_1.write(array[i]+'\n')
+        sys_1.close
+        array.clear()
+
+
+        with open("dia.txt", 'r') as dia:
+            for line in dia:
+                array.insert(0,line.strip())
+        dia.close
+        dia_1 = open('dia_1.txt','w')
+        for i in range (0, len(array)):
+            dia_1.write(array[i]+'\n')
+        dia_1.close
+        array.clear()
+
+
+        with open("pulse.txt", 'r') as pulse:
+            for line in pulse:
+                array.insert(0,line.strip())
+        pulse.close
+        pulse_1 = open('pulse_1.txt','w')
+        for i in range (0, len(array)):
+            pulse_1.write(array[i]+'\n')
+        pulse_1.close
+        array.clear()
+
+        with open("weather_pressure.txt", 'r') as wpr:
+            for line in wpr:
+                array.insert(0,line.strip())
+        wpr.close
+        wpr_1 = open('weather_pressure_1.txt','w')
+        for i in range (0, len(array)):
+            wpr_1.write(array[i]+'\n')
+        wpr_1.close
+        array.clear()
+
 
     def push_Settings_clicked(self):
-        pass
+        self.openWindow()
+        # self.setupUi.lineEdit_city 
+    
+    def push_About_clicked(self):
+        self.openWindow2()
+
+    def push_Graph_clicked(self):  
+        with open('date.txt', 'r') as filedata:
+        # Read the contents of the file and split it by newline character
+            data_list = filedata.read().split('\n')
+        filedata.close()
+
+        # with open('sys.txt', 'r') as filesys:
+        # # Read the contents of the file and split it by newline character
+        #     int_list_sys =[]
+        #     sys_list = [int(value) for value in filesys.read().split('\n')]
+        #     print(sys_list)
+        # filesys.close()
+    
+
+
+        # Open the file
+        with open('sys.txt', 'r') as filesys:
+            contents_sys = filesys.readlines()
+
+            # Create an empty array to store the data
+            data = []
+
+            # Loop through each line of the file and append to the array
+            for line in contents_sys:
+            # Remove the newline character from the end of the line
+                line = line.strip()
+                # Append the line to the array
+                data.append(line)
+
+            # Print the contents of the array
+        print(data)
+
+
+
+        # with open('dia.txt', 'r') as filedia:
+        # # Read the contents of the file and split it by newline character
+        #     dia_list = filedia.read().split('\n')
+        # filedia.close()
+
+# '        with open('weather_pressure.txt', 'r') as fileweather:
+#         # Read the contents of the file and split it by newline character
+#             weather_list = fileweather.read().split('\n')
+#         fileweather.close()'
+
+
+        x = data_list
+        y1 = data
+        #y2 = dia_list
+        #y3 = weather_list
 
 
 
 
-# def weather_pressure(self):                                     # OpenWeatherMap ilmapainetietoa hakeminen
-owm =pyowm.OWM('f8c43bbd601d39c177afabec2d050d04')
-mgr = owm.weather_manager()
-weather_pressure = mgr.weather_at_place('Helsinki').weather.pressure  # 'weather', not 'observation'
-pressure_value=weather_pressure['press']
-# return pressure_value
+        plt.title('Blood Pressure & Pulse')
+        plt.xlabel(xlabel='Date')
+        plt.xticks(rotation=45)
+        plt.ylabel(ylabel='Value')
+        plt.plot(x, y1, label='sys', color='red')
+
+        plt.grid()
+        plt.draw()
+        plt.legend(loc='upper left')
+        plt.show()
+
+
 
 
 if __name__ == "__main__":
